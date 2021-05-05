fba49a0fc725ecbdc8abc2b6dda14414ac62cc4b
buingoclam147 <buingoclam00@gmail.com>
2021-05-05 10:33:46 +0700

    POST method
    day 5 - cách sử dụng post *lưu ý: phải thêm middleware urlencoded

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const { query } = require('express')
+const { deprecate } = require('util')
 
 const app = express()
 const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')));
+//middleware dùng cho clien và thư viện 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 //http logger
 app.use(morgan('combined'))
 //template engine
-app.engine('hbs', handlebars({extname: '.hbs'}));
+app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
 // console.log('PATH:', path.join(__dirname, 'resources/views'))
@@ -26,7 +30,10 @@ app.get('/news', (req, res) => {
 app.get('/search', (req, res) => {
   res.render('search');
 })
-
+app.post('/search', (req, res) => {
+  console.log(req.body)
+  res.send('');
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
