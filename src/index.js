e90c4692132198eb89be1302467d6cf1e618ad47
buingoclam147 <buingoclam00@gmail.com>
2021-05-06 23:06:53 +0700

    debug
    day 6 - debug lại một số lỗi

--- src/index.js
+++ src/index.js
@@ -23,11 +23,11 @@ app.use(morgan('combined'));
 //template engine
 app.engine('hbs', handlebars({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 // console.log('PATH:', path.join(__dirname, 'resources/views'))
 //routes init
 route(app);
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`App listening at http://localhost:${port}`);
 });
