6ac18b17b736f58f80c99b85cc5ea5a3efe9ea08
buingoclam147 <buingoclam00@gmail.com>
2021-05-10 14:50:20 +0700

    Delete course
    day 9- xóa 1 khóa học k cần tạo page khác chỉ cần lấy đường dẫn đó ra có cái id của cái cần xóa là được

--- src/app/controllers/courseController.js
+++ src/app/controllers/courseController.js
@@ -39,11 +39,15 @@ class CourseController {
     }
     //[PUT] courses/:id
     update(req, res, next) {
-        Course.updateOne({ _id: req.params.id }, req.body) 
+        Course.updateOne({ _id: req.params.id }, req.body)
             .then(() => res.redirect('/'))
-            .catch(error => {
-
-            })
+            .catch(next)
+    }
+    //[DELETE] courses/:id
+    delete(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => res.redirect('back'))
+            .catch(next)
     }
 }
-module.exports = new CourseController;
+module.exports = new CourseController();
