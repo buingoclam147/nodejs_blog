8cef711db906ac0d87e33cec630c1bd5264c5453
buingoclam147 <buingoclam00@gmail.com>
2021-05-11 22:47:34 +0700

    Deleted count documents
    day 10 - sử dụng promise.all để bỏ các promise vào chung sử dụng kết quả chung vào1 page

--- src/app/controllers/meController.js
+++ src/app/controllers/meController.js
@@ -1,25 +1,43 @@
 const Course = require('../models/course')
 const { mutipleMongooseToObject } = require('../../util/mongoose')
 class meController {
+
+
     //[GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Course.find({}).then((courses)=>{
-            res.render('me/stored-courses',{
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+        .then(([courses, deletedCount]) => {
+            res.render('me/stored-courses', {
+                deletedCount,
                 courses: mutipleMongooseToObject(courses)
-            } )
+            })
+            .catch(next);
         })
-        .catch(next);
-       
+
+
+        // Course.countDocumentsDeleted()
+        //     .then((deletedCount) => {
+        //         console.log(deletedCount);
+        //     })
+        //     .catch(next);
+
+        // Course.find({}).then((courses) => {
+        //     res.render('me/stored-courses', {
+        //         courses: mutipleMongooseToObject(courses)
+        //     })
+        // })
+        //     .catch(next);
+
     }
     //[GET] /me/trash/courses
     trashCourses(req, res, next) {
-        Course.findDeleted({}).then((courses)=>{
-            res.render('me/trash-courses',{
+        Course.findDeleted({}).then((courses) => {
+            res.render('me/trash-courses', {
                 courses: mutipleMongooseToObject(courses)
-            } )
+            })
         })
-        .catch(next);
-       
+            .catch(next);
+
     }
 }
 module.exports = new meController;
